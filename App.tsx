cd7e2be11a622ca29286f7e004c47e1018139bef
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-09-26 16:40:44 +0200

    update App for Routes

--- App.tsx
+++ App.tsx
@@ -4,19 +4,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
 import Auth from './src/screens/Auth';
 import { AuthProvider } from './src/providers/AuthContext';
+import Routes from './src/router';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AuthProvider>
-        <Stack.Navigator initialRouteName="Auth">
-          <Stack.Screen name="Auth" component={Auth} options={{ headerShown: false }} />
-          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </AuthProvider>
-    </NavigationContainer>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 }
 
