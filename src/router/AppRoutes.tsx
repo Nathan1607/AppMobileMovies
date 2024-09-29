12f74809561333ae3a8412c78e62894513f11347
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-09-29 15:44:09 +0200

    clean icon for main root

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -6,7 +6,7 @@ import Profile from '../screens/Profile';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHeart, faHome, faP, faPeopleArrows, faPerson, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faHome, faPerson, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Tab = createBottomTabNavigator();
 
