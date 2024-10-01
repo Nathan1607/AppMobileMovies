2ae92ac4a4a1f4c79f8b4795a1798614cb94042d
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-10-01 13:29:42 +0200

    update style barCategory

--- src/components/BarCategory.tsx
+++ src/components/BarCategory.tsx
@@ -5,90 +5,93 @@ import { ScrollView } from 'react-native-gesture-handler';
 
 export default () => {
 
-    const [selectedCategory, setSelectedCategory] = useState('All'); // "All" est sélectionné par défaut
+    const [selectedCategory, setSelectedCategory] = useState('All');
 
     return (
         <View style={styles.container}>
-            <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={styles.scrollViewContent}
-            >
-                <TouchableOpacity
-                    style={[
-                        styles.button,
-                        selectedCategory === 'All' && styles.selectedButton // Appliquer le style si "All" est sélectionné
-                    ]}
-                    onPress={() => setSelectedCategory('All')} // Met à jour la sélection
+            <View style={styles.scrollViewWrapper}>
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.scrollViewContent}
+                    contentInset={{ left: 10, right: 10 }}
                 >
-                    <Text style={[
-                        styles.buttonText,
-                        selectedCategory === 'All' && styles.selectedButtonText // Appliquer style texte si sélectionné
-                    ]}>
-                        All
-                    </Text>
-                </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.button,
+                            selectedCategory === 'All' && styles.selectedButton
+                        ]}
+                        onPress={() => setSelectedCategory('All')}
+                    >
+                        <Text style={[
+                            styles.buttonText,
+                            selectedCategory === 'All' && styles.selectedButtonText
+                        ]}>
+                            All
+                        </Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity
-                    style={[
-                        styles.button,
-                        selectedCategory === 'Romance' && styles.selectedButton // Appliquer le style si "Romance" est sélectionné
-                    ]}
-                    onPress={() => setSelectedCategory('Romance')}
-                >
-                    <Text style={[
-                        styles.buttonText,
-                        selectedCategory === 'Romance' && styles.selectedButtonText
-                    ]}>
-                        Romance
-                    </Text>
-                </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.button,
+                            selectedCategory === 'Romance' && styles.selectedButton
+                        ]}
+                        onPress={() => setSelectedCategory('Romance')}
+                    >
+                        <Text style={[
+                            styles.buttonText,
+                            selectedCategory === 'Romance' && styles.selectedButtonText
+                        ]}>
+                            Romance
+                        </Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity
-                    style={[
-                        styles.button,
-                        selectedCategory === 'Sport' && styles.selectedButton // Appliquer le style si "Sport" est sélectionné
-                    ]}
-                    onPress={() => setSelectedCategory('Sport')}
-                >
-                    <Text style={[
-                        styles.buttonText,
-                        selectedCategory === 'Sport' && styles.selectedButtonText
-                    ]}>
-                        Sport
-                    </Text>
-                </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.button,
+                            selectedCategory === 'Sport' && styles.selectedButton
+                        ]}
+                        onPress={() => setSelectedCategory('Sport')}
+                    >
+                        <Text style={[
+                            styles.buttonText,
+                            selectedCategory === 'Sport' && styles.selectedButtonText
+                        ]}>
+                            Sport
+                        </Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity
-                    style={[
-                        styles.button,
-                        selectedCategory === 'Kids' && styles.selectedButton // Appliquer le style si "Kids" est sélectionné
-                    ]}
-                    onPress={() => setSelectedCategory('Kids')}
-                >
-                    <Text style={[
-                        styles.buttonText,
-                        selectedCategory === 'Kids' && styles.selectedButtonText
-                    ]}>
-                        Kids
-                    </Text>
-                </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.button,
+                            selectedCategory === 'Kids' && styles.selectedButton
+                        ]}
+                        onPress={() => setSelectedCategory('Kids')}
+                    >
+                        <Text style={[
+                            styles.buttonText,
+                            selectedCategory === 'Kids' && styles.selectedButtonText
+                        ]}>
+                            Kids
+                        </Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity
-                    style={[
-                        styles.button,
-                        selectedCategory === 'Horror' && styles.selectedButton // Appliquer le style si "Horror" est sélectionné
-                    ]}
-                    onPress={() => setSelectedCategory('Horror')}
-                >
-                    <Text style={[
-                        styles.buttonText,
-                        selectedCategory === 'Horror' && styles.selectedButtonText
-                    ]}>
-                        Horror
-                    </Text>
-                </TouchableOpacity>
-            </ScrollView>
+                    <TouchableOpacity
+                        style={[
+                            styles.button,
+                            selectedCategory === 'Horror' && styles.selectedButton
+                        ]}
+                        onPress={() => setSelectedCategory('Horror')}
+                    >
+                        <Text style={[
+                            styles.buttonText,
+                            selectedCategory === 'Horror' && styles.selectedButtonText
+                        ]}>
+                            Horror
+                        </Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </View>
         </View>
     );
 };
@@ -98,26 +101,32 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         alignItems: 'center',
     },
+    scrollViewWrapper: {
+        width: '90%',
+        backgroundColor: '#333333',
+        borderRadius: 20,
+        overflow: 'hidden',
+    },
     scrollViewContent: {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        paddingHorizontal: 10,
-        backgroundColor: '#fff',
+        paddingHorizontal: 0,
+        paddingLeft: 0
     },
     button: {
         borderRadius: 20,
         marginHorizontal: 5,
         paddingHorizontal: 15,
         paddingVertical: 10,
-        backgroundColor: '#E0E0E0',
+        backgroundColor: 'transparent',
     },
     buttonText: {
-        color: '#000',
+        color: '#fff',
         fontWeight: 'bold',
     },
     selectedButton: {
-        backgroundColor: '#FFD700',
+        backgroundColor: '#fff',
     },
     selectedButtonText: {
         color: '#000',
