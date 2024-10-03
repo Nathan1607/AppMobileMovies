1a8ce84402f62e600dafdff5ad56eb5a02994318
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-10-03 21:19:31 +0200

    test MovieCardDetail

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, View } from "react-native";
 import React, { useContext } from "react";
 import { Text } from "react-native-elements";
-
+import MovieCardDetail from "../components/MovieCardDetail";
 
 export default ({navigation} : any) => {
     
