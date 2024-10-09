ad61b8f446b7f269274f6b67663cac234dbc46cb
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-10-09 13:27:08 +0200

    Start page Home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,18 +4,16 @@ import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BarCategory from "../components/BarCategory";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import Button from "../components/Button";
+
 
 export default () => {
     
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
             <SafeAreaView style={styles.container}>
-                <BarCategory />
-                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                        <Text h1>Home</Text>
-                        <Button text="Go to Search" color="#f00" onPress={() => {}} />
-                    </View>
+                <View>
+                    <BarCategory />
+                </View>                
             </SafeAreaView>
         </GestureHandlerRootView>
     )
