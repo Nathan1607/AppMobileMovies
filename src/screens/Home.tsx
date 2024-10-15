d978cf8a198ee1c32cc0d1147aeb471e763f4f84
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-10-15 11:57:55 +0200

    install snap-carousel / add api

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import { Dimensions, Image, ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BarCategory from "../components/BarCategory";
@@ -8,12 +8,57 @@ import Carousel from "react-native-snap-carousel";
 
 export default () => {
     
+    const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODZhOGEwOWJiZmU2MWUwNjIxNWUzMmQyMDllYzE5YyIsIm5iZiI6MTcyODk3ODU0Ni4xNzE1MTUsInN1YiI6IjY3MGE5N2MwMzdkODZkNTIwYmIwODQ2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.49O9dbuRFT3Q32zn15USk97k9AMplfp0d0YwIi5TG18"
+    const [movies, setMovies] = React.useState([]);
+
+    useEffect(() => {
+        getMovieCarousel();
+    }, []);
+
+    async function getMovieCarousel(){
+        try {
+            const response = await fetch('https://api.themoviedb.org/3/movie/popular?page=1',
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + token
+                }
+            });
+            if(response.ok) {
+                const data = await response.json();
+                console.log(data);
+                setMovies(data.results.slice(0, 4));
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    const renderItem = ({ item } : { item: any }) => {
+        const posterUrl = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
+        return (
+            <View style={styles.carouselItem}>
+                <Image
+                    source={{ uri: posterUrl }}
+                    style={styles.posterImage}
+                />
+            </View>
+        );
+    }
       
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
             <SafeAreaView style={styles.container}>
                 <View>
                     <BarCategory />
+                    <Carousel
+                        data={movies}
+                        renderItem={renderItem}
+                        sliderWidth={Dimensions.get('window').width}
+                        itemWidth={200}
+                        loop={true}
+                    />
                 </View>                
             </SafeAreaView>
         </GestureHandlerRootView>
@@ -25,4 +70,13 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#000',
     },
+    carouselItem: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    posterImage: {
+        width: 300,
+        height: 450,
+        borderRadius: 10,
+    }
 });
