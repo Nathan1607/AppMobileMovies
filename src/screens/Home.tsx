170b7a000e4e2f646f8f6c8e9fc01f72a085f85d
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-10-25 19:00:47 +0200

    create carousel for marvel movies

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,12 +2,13 @@ import {
   GestureResponderEvent,
   Image,
   StyleSheet,
+  Text,
   View,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import BarCategory from '../components/BarCategory';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {GestureHandlerRootView, ScrollView} from 'react-native-gesture-handler';
 import Button from '../components/Button';
 import { faHeart, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
@@ -15,15 +16,15 @@ export default () => {
     
   const token =
     'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODZhOGEwOWJiZmU2MWUwNjIxNWUzMmQyMDllYzE5YyIsIm5iZiI6MTcyODk3ODU0Ni4xNzE1MTUsInN1YiI6IjY3MGE5N2MwMzdkODZkNTIwYmIwODQ2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.49O9dbuRFT3Q32zn15USk97k9AMplfp0d0YwIi5TG18';
-  
+
   interface Movie {
     poster_path: string;
   }
 
-  const [movie, setMovie] = useState<Movie | null>(null);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
-    const fetchMovie = async () => {
+    const fetchMovies = async () => {
       try {
         const response = await fetch(
           'https://api.themoviedb.org/3/movie/popular?page=1',
@@ -37,14 +38,14 @@ export default () => {
         );
         if (response.ok) {
           const data = await response.json();
-          setMovie(data.results[0]);
+          setMovies(data.results.slice(0, 10));
         }
       } catch (error) {
         console.error('Error:', error);
       }
     };
 
-    fetchMovie();
+    fetchMovies();
   }, []);
 
   return (
@@ -63,18 +64,40 @@ export default () => {
             color="#333"
             onPress={function (event: GestureResponderEvent): void {
               throw new Error('Function not implemented.');
-            } }   
+            }}   
           />
           <Button
             text="Details"
             color="#FFC107" 
             onPress={function (event: GestureResponderEvent): void {
               throw new Error('Function not implemented.');
-            } }          
+            }}          
             />
         </View>
+        
+        <View>
+          <Text style={styles.textMarvel}>Marvel Studios</Text>
+          <Text style={styles.linkMarvel}>See more</Text>
 
-        {/* Pour réaliser le Marvel Studios  -> faire un scrollview horizontal d'une réplication de view */}
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            style={styles.carouselContainer}
+          >
+            {movies.map((movie, index) => (
+              <Image
+                key={index}
+                source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }}
+                style={[
+                  styles.posterImage,
+                  index === movies.length - 1 ? { marginRight: 30 } : {},
+                ]}
+              />
+            ))}
+              {/* <View style={{ width: 20 }} /> */}
+
+          </ScrollView>
+        </View>
       </SafeAreaView>
     </GestureHandlerRootView>
   );
@@ -102,5 +125,31 @@ const styles = StyleSheet.create({
     marginTop: -150,
     width: '100%',
     height: 50,
+  },
+  textMarvel : {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    marginTop: 30,
+    marginLeft: 20,
+  },
+  linkMarvel: {
+    color: 'yellow',
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlign: 'right',
+    marginTop: -20,
+    marginRight: 20,
+  },
+  carouselContainer: {
+    marginTop: 10,
+    paddingHorizontal: 20,
+  },
+  posterImage: {
+    width: 120,
+    height: 180,
+    borderRadius: 10,
+    marginRight: 15,
   },
 });
