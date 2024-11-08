0c6b01a8cc9befcc108949855d20a0e763957834
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-11-08 18:27:17 +0100

    update page Auth

--- src/router/AuthRoutes.tsx
+++ src/router/AuthRoutes.tsx
@@ -7,7 +7,7 @@ const Stack = createNativeStackNavigator();
 const AuthRoutes = () => {
   return (
     <Stack.Navigator initialRouteName="SignIn">
-      <Stack.Screen name="SignIn" component={Auth} />
+      <Stack.Screen name="SignIn" component={Auth}options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 };
--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -1,27 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, View, SafeAreaView, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
-import { useContext } from 'react';
 import { useAuth } from '../providers/AuthContext';
 import { AuthContext } from '../providers/AuthContext';
 
-export default ({navigation}: any) => {
+export default () => {
 
     const { login } = useAuth();
-
     const { username, setUsername } = useContext(AuthContext);
     const { password, setPassword } = useContext(AuthContext);
-    const { isAuthenticated } = useContext(AuthContext);
 
     const handleLogin = async () => {
         await login(username, password);
     }
 
     return (
-        <SafeAreaView>
-            <View style={{ padding: 20 }}>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.innerContainer}>
                 <Text style={styles.title}>Authentication</Text>
-                <TextInput placeholder="John Doe" style={styles.input} onChangeText={setUsername} />
-                <TextInput placeholder="Password" style={styles.input} onChangeText={setPassword} secureTextEntry />
+                <TextInput placeholder="John Doe" placeholderTextColor="#aaa" style={styles.input} onChangeText={setUsername} />
+                <TextInput placeholder="Password" placeholderTextColor="#aaa" style={styles.input} onChangeText={setPassword} secureTextEntry />
                 <TouchableOpacity style={styles.button} onPress={handleLogin}>
                     <Text style={styles.textButton}>Login</Text>
                 </TouchableOpacity>
@@ -31,33 +28,44 @@ export default ({navigation}: any) => {
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+    },
+    innerContainer: {
+        padding: 20,
+        flex: 1,
+        justifyContent: 'center',
+    },
     title: {
-        color: 'black',
+        color: '#fff',
         fontSize: 24,
         fontWeight: 'bold',
         textAlign: 'center',
         marginBottom: 100,
         marginTop: 50,
-      },
+    },
     input: {
-        backgroundColor: 'white',
-        borderColor: 'black',
+        backgroundColor: '#333',
+        color: '#fff',
+        borderColor: '#555',
         borderWidth: 1,
         padding: 10,
         marginBottom: 50,
+        borderRadius: 5,
         marginVertical: 10,
-      },
+    },
     button: {
-        backgroundColor: 'gray',
+        backgroundColor: '#666',
         padding: 10,
         borderRadius: 5,
         borderWidth: 1,
+        borderColor: '#555',
         marginVertical: 10,
         marginTop: 50,
     },
     textButton: {
-        color: 'white',
+        color: '#fff',
         textAlign: 'center',
     }
 });
-
